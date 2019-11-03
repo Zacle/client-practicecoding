5d3a18d669fc2a24028a7694fc9f9e708dd60330
zacle <zacharienziuki@gmail.com>
2019-11-03 22:07:44 +0200

    Updated Yarn lockfile

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-const {
-  PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD
-} = require('next/constants')
+const { PHASE_PRODUCTION_SERVER } =
+      process.env.NODE_ENV === 'development'
+      ? {}
+      : !process.env.NOW_REGION
+      ? require('next/constants')
+      : require('next-server/constants');
 
 // Fixes npm packages that depend on `fs` module
 const nextConfig = { webpack: config => ({ ...config, node: { fs: 'empty' } }) }
